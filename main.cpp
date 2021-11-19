db441c0f49a96328de525ec88f14755c6926aa20
Vikash20-12 <vikashsinghtomar21@gmail.com>
2021-11-19 13:48:17 +0530

    vectors done.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,19 @@ using namespace std;
 // PAIRS
 void stlPair(){
 
-    // pair<int, int> p = {1, 3};
-    // cout<<p.first<<" "<<p.second<<endl;
+    pair<int, int> p = {1, 3};
+    cout<<"\nPrinting general pairs: \n"<<p.first<<" "<<p.second<<endl;
     // // // prints  1 3
 
-    // pair<int, pair<int, int>> p = {1, {3, 4}};
-	// cout <<p.first<<" "<<p.second.first<<" "<<p.second.second<<endl;
+    pair<int, pair<int, int>> p1 = {1, {3, 4}};
+	cout <<"\nPrinting nested pairs: \n"<<"{"<<p1.first<<" "<<", {"<<p1.second.first<<" , "<<p1.second.second<<"}}"<<endl;
     // // prints 1 4 3
 
-    // pair<int, int> arr[]={ {1,2}, {3,4}, {5,6}};
-    // for(int i=0; i<3; i++){
-    //     cout<<arr[i].first<<"  "<<arr[i].second<<endl;
-    // }
+    cout<<"\nPrinting array of pairs\n";
+    pair<int, int> arr[]={ {1,2}, {3,4}, {5,6}};
+    for(int i=0; i<3; i++){
+        cout<<"{"<<arr[i].first<<" , "<<arr[i].second<<"}"<<endl;
+    }
     // prints 
     // 1 2
     // 3 4
@@ -47,17 +48,26 @@ void stlVector(){
     // rbegin() will point to the last index.
     auto a = v1.end() - 1;
     // end() will point to one position ahead of last element index.
-    cout<<*(x)<<" "<<*(y)<<" "<<*(z)<<" "<<*(a)<<" "<<endl;
-    // for(auto it=v1.begin(); it!=v1.end(); it++)
-    // {
-    //     cout<<*(it)<<" "<<endl;
-    // }
-    //this is just a test
+    cout<<" \n begin()- will point to the first index. \nx = "<<*(x)<<"\n rend()- will point to memory address before the first element.  \ny = "<<*(y)<<"\n rbegin()- will point to the last index. \nz = "<<*(z)<<"\n end()- will point to one position ahead of last element index. \na = "<<*(a)<<"\n"<<endl;
+
+
+    for(auto it=v1.begin(); it!=v1.end(); it++)
+    {
+        cout<<*(it)<<" "<<endl;
+    }
+
+    vector<int>::iterator i = v1.begin();
+
+    cout<<"\nPrinting values of iterator"<<endl;
+    cout<<*(i)<<endl;
+    cout<<*(i+1)<<endl;
+    cout<<*(i+2)<<endl;
+    cout<<*(i+3)<<endl;
     
 }
 
 int main(){
-    // stlPair();
+    stlPair();
     stlVector();
 
     return 0;
